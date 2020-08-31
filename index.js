24a33ad03a5c8b914e3a799ffb171c80720fd5b5
Rkuangdev <RKuangDev@gmail.com>
2020-08-31 10:26:15 -0400

    added discord-side remove tweet feature

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Twitter = require('twit')
 const { send } = require('process')
 const client = new Discord.Client()
 const base64 = require("node-base64-image");
+const { Console } = require('console');
 var successChannel = "743989101157679286"
 require('dotenv').config()
 var author;
@@ -14,10 +15,14 @@ var twitterClient = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
+//discord and twitter associations (discord, twitter)
+var associations = new Map()
+
 const sendToLogs = (msg) => {
     let time = new Date()
     console.log(`[${time.toLocaleString('en-US', { timeZone: 'America/New_York' })}] ${msg}`)
 }
+
 
 client.on('ready', () => {
     sendToLogs(`Logged in as ${client.user.tag}!`)
@@ -27,6 +32,27 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.channel.id === successChannel) {
         if (message.attachments.size > 0) {
+            message.react('🗑')
+            let filter = (reaction, user) => {
+                return reaction.emoji.name === '🗑' && user.id === message.author.id;
+            };
+            message.awaitReactions(filter, { max: 1, time: 600000 })
+                .then(collected => {
+                    message.delete()
+                    twitterClient.post(`statuses/destroy/:id`, { id: associations.get(message.id) }, function (err, data, response) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                    associations.delete(message.id)
+                    message.channel.send({
+                        embed: {
+                            color: 16737913,
+                            description: `Deleted From Twitter :white_check_mark:`
+                        }
+                    })
+
+                })
             message.attachments.forEach(async image => {
                 author = message.author.username + "#" + message.author.discriminator
 
@@ -49,19 +75,20 @@ client.on('message', message => {
                         if (err) {
                             console.log(err)
                         }
-                        console.log(tweet.id);
+
+                        associations.set(message.id, tweet.id_str)
+
                         message.channel.send({
                             embed: {
                                 color: 4701043,
                                 description: `Posted To Twitter :white_check_mark:`
                             }
-                        });
+                        })
                     })
-
                 })
             })
         }
     }
 })
 
-client.login('Token')
+client.login('NzQ5NjU3NDMyMTQ1MjY0NzUx.X0vK4w.JWl4YqXbrRKNrW_jyRoH1nGEbEs')
